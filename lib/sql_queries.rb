b8441496d7ed4b11921a47f92d41127745201e80
Ty Bandachit <tbandachit@gmail.com>
2020-03-19 18:04:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,22 +7,26 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-"SELECT projects.title, SUM(amount) FROM pledges INNER JOIN projects ON pledges.project_id = projects.id GROUP BY projects.title"
+"SELECT projects.title, SUM(amount) FROM pledges 
+INNER JOIN projects 
+ON pledges.project_id = projects.id 
+GROUP BY projects.title"
 
 end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, SUM(AMOUNT) FROM pledges INNER JOIN users on pledges.user_id == users.id GROUP BY users.name"
+  "SELECT users.name, users.age, SUM(AMOUNT) FROM pledges 
+  INNER JOIN users on pledges.user_id == users.id 
+  GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
-    FROM projects
+  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects
     INNER JOIN pledges ON projects.id = pledges.project_id
     GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+    "SELECT users.name, sum(amount) FROM pledges JOIN users ON user_id =="
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
