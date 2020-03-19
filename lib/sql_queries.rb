c1e089342b413c7e465d896e70e767e1ec965ed6
Ty Bandachit <tbandachit@gmail.com>
2020-03-19 17:54:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(amount) FROM pledges INNER JOIN users on pledges.user_id == "
+  "SELECT projects.title, SUM(amount - projects.funding_goal) FROM pledges INNER JOIN projects ON pledges.project_id == project_id"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
